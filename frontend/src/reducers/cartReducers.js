074712875111b511eb0baf1974a632b21caf0b7b
alexomon018 <aleksa.mitic5859@gmail.com>
2020-11-10 13:54:24 +0100

    added addToCart and removeToCart functionalty

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
   if (action.type === CART_ADD_ITEM) {
@@ -17,6 +17,12 @@ export const cartReducer = (state = { cartItems: [] }, action) => {
         ...state,
         cartItems: [...state.cartItems, item],
       }
+    }
+  }
+  if (action.type === CART_REMOVE_ITEM) {
+    return {
+      ...state,
+      cartItems: state.cartItems.filter((x) => x.product !== action.payload),
     }
   }
   return state
