7df442c75577628a16a80460549ebbb972c4a405
alexomon018 <aleksa.mitic5859@gmail.com>
2020-11-14 17:22:28 +0100

    added shipping screen and updated cart reducer with shippingAddress

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
 function App() {
   return (
     <Router>
@@ -16,6 +17,7 @@ function App() {
       <main className='py-3'>
         <Container>
           <Route path='/login' component={LoginScreen}></Route>
+          <Route path='/shipping' component={ShippingScreen}></Route>
           <Route path='/register' component={RegisterScreen}></Route>
           <Route path='/profile' component={ProfileScreen}></Route>
           <Route path='/product/:id' component={ProductScreen}></Route>
